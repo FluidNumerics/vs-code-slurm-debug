68d1e9a80e29c68cbe2589c4be1a6e49db3e7d36
garrett <garrett@fluidnumerics.com>
2023-07-21 21:03:53 +0000

    Added lines to cancel job on keyboard interrupt

--- .vscode/launch-batch.sh
+++ .vscode/launch-batch.sh
@@ -4,18 +4,21 @@ JOB=$RANDOM
 TEMP_FILE="/tmp/vscode-sbatch-script.$JOB.slurm"
  
 echo "Temporary job file: $TEMP_FILE" # show file name
-
 echo "Current running file: $1" # show which script is being run
+
+# Cancel job on Ctrl+C Keyboard Interrupt
+JOBNAME="vscode-$JOB"
+trap "scancel -n $JOBNAME; exit 130" SIGINT
 
 cat >$TEMP_FILE << EOL
 #!/bin/bash
 #SBATCH --nodelist=rcclive-c260-sm-0
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --job-name=vscode-$JOB  
+#SBATCH --job-name=$JOBNAME  
 #SBATCH --time=1:00:00               
-#SBATCH -o vscode-$JOB.log                               
-#SBATCH -e vscode-$JOB.log  
+#SBATCH -o $JOBNAME.log                               
+#SBATCH -e $JOBNAME.log  
 #SBATCH --get-user-env  
 
 source \${HOME}/.bashrc
@@ -32,7 +35,7 @@ sbatch --get-user-env $TEMP_FILE
 
 echo 'Waiting for Slurm job to begin..'
 while true; do
- export JOB_STATUS=$(squeue -n vscode-$JOB --format="%.2t" | tail -n1 | xargs)
+ export JOB_STATUS=$(squeue -n $JOBNAME --format="%.2t" | tail -n1 | xargs)
  echo "Job Status : $JOB_STATUS"
  if [ "$JOB_STATUS" == "R" ]; then
    echo "Job started!"
